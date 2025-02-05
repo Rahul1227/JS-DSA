382d3a3a1c796dcc20fa78c064f5e83e12f2cbc7
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-02-05 11:12:03 +0530

    Time: 54 ms (34.73%) | Memory: 76.9 MB (17.27%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -1,57 +1,33 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var longestConsecutive = function(nums) {
-//     let hashMap=[];
-//     for(let i=0; i<nums.length; i++){
-//         hashMap[nums[i]]=1;
-//     }
-
-//     let maxSequenceLength=0;
-//     let counter=0;
-
-//     for(let i=0; i<hashMap.length; i++){
-//         if(hashMap[i]===1){
-//             counter++;
-//         }else{
-//             if(maxSequenceLength<counter){
-//                 maxSequenceLength=counter;
-//                 counter=0;
-//             }
-//         }
-//     }
-//     if(maxSequenceLength===0){
-//         maxSequenceLength=counter;
-//     }
-
-//     return maxSequenceLength;
-    
-// };
-
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var longestConsecutive = function(nums) {
-    let hashMap = {};
-    for (let i = 0; i < nums.length; i++) {
-        hashMap[nums[i]] = true;
+    let hashMap=new Map();
+    let maxCounter=0;
+
+    for(let i=0; i<nums.length; i++){
+        hashMap.set(nums[i],1);
     }
 
-    let maxSequenceLength = 0;
 
-    for (let num in hashMap) {
-        // Check if it's the start of a sequence
-        if (!hashMap[num - 1]) {
-            let currentNum = Number(num);
-            let counter = 0;
+    hashMap.forEach((value,key)=>{
 
-            while (hashMap[currentNum]) {
-                counter++;
-                currentNum++;
-            }
-            
-            maxSequenceLength = Math.max(maxSequenceLength, counter);
+        if(!hashMap.has(key-1)){
+        let counter=0;
+        
+
+        while(hashMap.has(key++)){
+            counter++;
+        }
+
+        if(counter>maxCounter){
+            maxCounter=counter;
         }
     }
 
-    return maxSequenceLength;
+    })
+
+    return maxCounter;
+    
 };
