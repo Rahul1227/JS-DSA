256ac9c1ed1dedf0a16734d77ad0a8e0c4b7f7a2
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-02-10 18:32:31 +0530

    Time: 6 ms (94.36%) | Memory: 50.6 MB (91.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,27 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    // Should return `false` if lengths are not equal
-    if (s.length !== t.length) return false;
-
-    let letterArray = new Array(26).fill(0);
-
-    // Increment counts for characters in s
-    for (let i = 0; i < s.length; i++) {
-        letterArray[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    if(s.length!==t.length) return false;
+    let arr=new Array(26).fill(0);
+    for(let i=0; i<s.length; i++){
+        arr[s.charCodeAt(i)-'a'.charCodeAt(0)]++;
+        arr[t.charCodeAt(i)-'a'.charCodeAt(0)]--;
+    
     }
 
-    // Decrement counts for characters in t
-    for (let i = 0; i < t.length; i++) {
-        letterArray[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
-    }
+    
+    return arr.every(x=> x===0);
 
-    // Calculate the sum of the array to check if all counts return to zero
-    let sum = 0;
-    letterArray.forEach(num => {
-        sum += Math.abs(num);  // Use absolute value to ensure that positives and negatives do not cancel out
-    });
-
-    // If sum is zero, all counts are balanced and the strings are anagrams
-    return sum === 0;
 };
